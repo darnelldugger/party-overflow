3c3ed0a4cb2a04c1d949cfcc419c4b503150c285
Darnell Dugger <darnelldugger@gmail.com>
2022-06-24 19:18:55 -0400

    create sign up page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Nav from '../components/Nav/Nav'
+import SignUp from '../pages/Auth/SignUp'
 
 //Services
 
@@ -22,7 +23,7 @@ const App = () => {
         />
 
         <Route path='/signup'
-          element={<h1>Sign Up</h1>}
+          element={<SignUp />}
         />
 
         <Route path='/posts'
---
+++ src/pages/Auth/SignUp.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import '../../styles/Auth.css'
+
+
+// Assets
+import cat from '../../assets/avatars/cat.png' //<= included in starter code
+
+// Services
+// we will import a sign up service momentarily
+import { signup } from '../../services/authService'
+
+const SignUp = (props) => {
+
+  //state for tracking errors
+  const [msg, setMsg] = useState('')
+
+  //form state
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    avatar: cat
+  })
+
+  const handleChange = (e) => {
+    setMsg('')
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      console.log('it works')
+    } catch (error) {
+      setMsg(error.message) //caught an error? add it to error state
+    }
+  }
+
+  return (
+    <div className="signup-page">
+
+      <div className='left-container'>
+
+        <div className='form-container'>
+          <div className="title-container">
+            <h1>Create an Account</h1>
+            <h3>Social media for developers</h3>
+          </div>
+          <form className="register-form">
+            <input
+              required
+              name="name"
+              type="text"
+              autoComplete="off"
+              placeholder="Username"
+              onChange={handleChange}
+              value={formData.name}
+            />
+            <input
+              required
+              name="email"
+              type="email"
+              autoComplete="off"
+              placeholder="Email"
+              onChange={handleChange}
+              value={formData.email}
+            />
+            <input
+              required
+              name="password"
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              onChange={handleChange}
+              value={formData.password}
+            />
+
+            <button
+              autoComplete="off"
+              id="avatar-button"
+              type="button"
+            >Select Avatar</button>
+
+            <button
+              autoComplete="off"
+              id="submit-button"
+              type="submit"
+            >SIGN UP</button>
+          </form>
+        </div>
+
+      </div>
+
+      <div className="right-container">
+        Animation Here
+      </div>
+
+    </div>
+  )
+}
+
+export default SignUp
