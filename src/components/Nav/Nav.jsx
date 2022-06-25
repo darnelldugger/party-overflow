8961e4055bd862e744541bab70253a1a39fd367d
Darnell Dugger <darnelldugger@gmail.com>
2022-06-24 21:11:03 -0400

    add logout to nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../styles/Nav.css'
+
 import { NavLink } from 'react-router-dom'
 import LogoDesktop from '../../assets/logo/logo-desktop.png'
 
@@ -7,8 +8,15 @@ const Nav = (props) => {
   return (
     <nav className="nav-bar">
       <NavLink id="logo" to='/posts'><img src={LogoDesktop} alt="disco-ball" /></NavLink>
-      <NavLink to='/signup'>Sign Up</NavLink>
-      <NavLink to='/signin'>Sign In</NavLink>
+      {props.user
+        ? <>
+          <NavLink to="/" onClick={props.handleLogout}>Logout</NavLink>
+        </>
+        : <>
+          <NavLink to='/signup'>Sign Up</NavLink>
+          <NavLink to='/signin'>Sign In</NavLink>
+        </>
+      }
     </nav>
   )
 }
